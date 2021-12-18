a385214ab90cc50f04758f071237c262aac1e58b
Hideki Igarashi <hideki.develop@gmail.com>
2021-12-18 22:11:28 +0900

    style: Fix script style

--- lib/utils.bash
+++ lib/utils.bash
@@ -25,14 +25,14 @@ TOOL_NAME="kubectl-kots"
 TOOL_DISPLAY_NAME="Replicated KOTS"
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
+  echo -e "asdf-${TOOL_NAME}: $*"
   exit 1
 }
 
 curl_opts=(-fsSL)
 
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+if [[ -n ${GITHUB_API_TOKEN:-} ]]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token ${GITHUB_API_TOKEN}")
 fi
 
 sort_versions() {
@@ -41,7 +41,7 @@ sort_versions() {
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
+  git ls-remote --tags --refs "${GH_REPO}" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
     sed 's/^v//'
 }
@@ -60,10 +60,10 @@ download_release() {
   platform="$(get_platform)"
   arch="$(get_arch)"
 
-  url="$GH_REPO/releases/download/v${version}/kots_${platform}_${arch}.tar.gz"
+  url="${GH_REPO}/releases/download/v${version}/kots_${platform}_${arch}.tar.gz"
 
   echo "* Downloading ${TOOL_DISPLAY_NAME} release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  curl "${curl_opts[@]}" -o "${filename}" -C - "${url}" || fail "Could not download ${url}"
 }
 
 install_version() {
@@ -71,8 +71,8 @@ install_version() {
   local version="$2"
   local install_path="$3"
 
-  if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
+  if [[ ${install_type} != "version" ]]; then
+    fail "asdf-${TOOL_NAME} supports release installs only"
   fi
 
   (
@@ -86,10 +86,10 @@ install_version() {
     test -x "${install_path}/bin/${TOOL_NAME}" ||
       fail "Expected ${install_path}/bin/${TOOL_NAME} to be executable."
 
-    echo "${TOOL_DISPLAY_NAME} $version installation was successful!"
+    echo "${TOOL_DISPLAY_NAME} ${version} installation was successful!"
   ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing ${TOOL_DISPLAY_NAME} $version."
+    rm -rf "${install_path}"
+    fail "An error occurred while installing ${TOOL_DISPLAY_NAME} ${version}."
   )
 }
 
